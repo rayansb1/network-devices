202e0a647403cd428ac6ad16c2766a77f9f48308
Rayan Bawazeer <136233403+rayansb1@users.noreply.github.com>
2025-02-06 23:52:40 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+import subprocess
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Backend is running on another laptop!"
+@app.route('/run_arp')
+def run_arp():
+    try:
+        result = subprocess.run(['arp', '-a'], capture_output=True, text=True)
+        return jsonify({"output": result.stdout})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
